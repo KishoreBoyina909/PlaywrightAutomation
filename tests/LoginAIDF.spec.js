5dc5d2706491296509f6f9ccf4b01638a65656c7
KishoreBoyina909 <kishoreboina.5@gmail.com>
2025-07-09 14:03:00 +0530

    Refactored login page with method-level structure

--- tests/LoginAIDF.spec.js
+++ tests/LoginAIDF.spec.js
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 import { validUser } from '../utils/testData';
-
+// Login to AIDF
 test('Login and navigate to Data Hub app', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.loginToAIDF(validUser.email, validUser.password);
