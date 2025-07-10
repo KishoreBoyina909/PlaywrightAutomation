b4fc16eb68b90d503a4fb23fb335fb4edd00bb0e
KishoreBoyina909 <kishoreboina.5@gmail.com>
2025-07-10 14:42:33 +0530

    Removed demo and unrelated spec files not part of AIDF app

--- tests/Locators.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'
-import { linkSync } from 'fs';
-
-test('Finding Locators', async ({ page }) => {
-    await page.goto('https://www.demoblaze.com/index.html');
-    await page.click('id=login2');
-    await page.locator('#loginusername').fill("Kishore Boyina");
-
-})
